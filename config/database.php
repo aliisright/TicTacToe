61e7697d4e0901a10a668257eebd27359a4b3136
Aliisright <alihasan.me@me.com>
2018-04-03 01:10:09 +0200

    heroku db config

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', $database),
             'username' => env('DB_USERNAME', $username),
